bf579cbaae917639c1cff1a8f1b86bfdb03dd2b8
Serge Aleksandrov <alserge.dct@gmail.com>
2018-02-19 11:36:12 -0500

    Implicit/explicit waits

--- java-example/src/test/java/myFirstSeleniumProject/AdminPanel.java
+++ java-example/src/test/java/myFirstSeleniumProject/AdminPanel.java
@@ -5,8 +5,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Point;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
@@ -16,11 +18,13 @@ public class AdminPanel {
   WebDriver driver;
   String l;
   String k, n, m;
+  WebDriverWait wait;
 
   @Before
 
   public void start(){
     driver = new ChromeDriver();
+    wait = new WebDriverWait(driver, 30);
     driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
   }
 
@@ -44,24 +48,25 @@ public class AdminPanel {
     System.out.println(l = driver.getTitle());
     Assert.assertEquals("Logotype | My Store", l);
     Assert.assertTrue(isElementPresent(By.xpath("//div/main/h1")));
-    Assert.assertTrue(isElementPresent(By.cssSelector("button.btn.btn-default[name=save]")));
-    driver.findElement(By.cssSelector("button.btn.btn-default[name=save]")).getText();
-    driver.findElement(By.cssSelector("button.btn.btn-default[name=save]")).isEnabled();
-    driver.findElement(By.cssSelector("button.btn.btn-default[name=save]")).getSize();
-    Point screen = driver.findElement(By.cssSelector("button.btn.btn-default[name=save]")).getLocation();
-    k = driver.findElement(By.cssSelector("button.btn.btn-default[name=save]")).getCssValue("background");
-    n = driver.findElement(By.cssSelector("button.btn.btn-default[name=save]")).getAttribute("scrollWidth");
+//    Assert.assertTrue(isElementPresent(By.cssSelector("button.btn.btn-default[name=save]")));
+//    driver.findElement(By.cssSelector("button.btn.btn-default[name=save]")).getText();
+//    driver.findElement(By.cssSelector("button.btn.btn-default[name=save]")).isEnabled();
+//    driver.findElement(By.cssSelector("button.btn.btn-default[name=save]")).getSize();
+//    Point screen = driver.findElement(By.cssSelector("button.btn.btn-default[name=save]")).getLocation();
+//    k = driver.findElement(By.cssSelector("button.btn.btn-default[name=save]")).getCssValue("background");
+//    n = driver.findElement(By.cssSelector("button.btn.btn-default[name=save]")).getAttribute("scrollWidth");
 
     System.out.println(k);
     System.out.println(n);
-    System.out.println(screen);
+    //System.out.println(screen);
   }
 
   boolean isElementPresent(By locator) {
     try {
-      driver.findElement(locator);
+      wait.until((WebDriver d) -> d.findElement(locator));  // implicit wait
+      //driver.findElement(locator);  // explicit wait
       return true;
-    } catch (NoSuchElementException ex) {
+    } catch (TimeoutException ex) {
       return false;
     }
   }
